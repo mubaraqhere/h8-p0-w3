3c2b4aa8f6a9e139dd645c356a4d3b88a450019e
mubaraqhere <mubaraqhere@gmail.com>
2020-03-10 16:15:42 +0700

    remove 's'

--- exercise-14.js
+++ exercise-14.js
@@ -7,7 +7,7 @@ function mengelompokkanAngka(arr) {
     }
     
     for(let j=0; j<arr.length; j++){
-        if(arr[j] % 3 === 0){   s
+        if(arr[j] % 3 === 0){
             hasil[2].push(arr[j])
         } else if(arr[j] % 2 !== 0){
             hasil[1].push(arr[j])
